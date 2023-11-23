b00817117e0d167808ddf3047aee4d0c2d4f03f2
Tymur Kosiak <us1gav@gmail.com>
2023-11-23 21:55:58 +0200

    Homework lesson29 create.

---
+++ src/main/java/edu/hillel/springmvclesson29/SpringMvcLesson29Application.java
@@ -0,0 +1,13 @@
+package edu.hillel.springmvclesson29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLesson29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcLesson29Application.class, args);
+    }
+
+}
